10c84c41fb306517b13c99be4f52c1c7532fdb75
BartVanDerBurg <bvanderburg@hotmail.com>
2025-10-11 16:45:27 +0200

    HomePage - Added other items

---
+++ src/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿
+// label active menu item
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname.toLowerCase().replace(/\/$/, "");
+
+    document.querySelectorAll(".navbar-nav .nav-link").forEach(link => {
+        if (!link.getAttribute("href") || link.getAttribute("href") === "#")
+            return;
+
+        const linkPath = new URL(link.href, window.location.origin).pathname.toLowerCase().replace(/\/$/, "");
+        if (linkPath === currentPath) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
