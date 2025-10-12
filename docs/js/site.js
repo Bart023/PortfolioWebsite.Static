fbeb5c6650af293695995882e4b56bf7580c140c
BartVanDerBurg <bvanderburg@hotmail.com>
2025-10-12 14:32:20 +0200

    Implemented theme to switch between light and dark mode

--- docs/js/site.js
+++ docs/js/site.js
@@ -15,3 +15,33 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// Toggle dark/light mode
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("toggleTheme");
+    const light = document.getElementById("theme-pill-light");
+    const dark = document.getElementById("theme-pill-dark");
+
+    let theme = localStorage.getItem("theme") || "light";
+    applyTheme(theme);
+
+    btn.addEventListener("click", () => {
+        theme = theme === "light" ? "dark" : "light";
+        localStorage.setItem("theme", theme);
+        applyTheme(theme);
+    });
+
+    function applyTheme(theme) {
+        document.documentElement.setAttribute("data-bs-theme", theme);
+        light.className = "";
+        dark.className = "";
+
+        if (theme === "light") {
+            light.classList.add("badge", "rounded-pill", "text-bg-warning");
+        } else {
+            dark.classList.add("badge", "rounded-pill", "text-bg-warning");
+        }
+
+        btn.querySelector("i").className = theme === "light" ? "bi bi-sun-fill me-2" : "bi bi-moon-fill me-2";
+    }
+});
